7205c736ca6fa9bdf3f6eb892403c280cbd3262b
divineo <59251464+divineo@users.noreply.github.com>
2020-06-01 15:00:26 +0530

    Update StringCalculator for -ve no. and no. > 1000

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,24 +1,36 @@
 public class StringCalculator {
     public int add(String input) {
+        
         String[] numbers = input.split(",|\n");
+        
         if (input.isEmpty()) {
             return 0;
         }
+        
         else if (numbers.length > 1) {
             return getSum(numbers);
         }
-        return getInt(input);
+        return stringToInt(input);
     }
     
     private int getSum(String[] numbers) {
         int sum = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            sum += getInt(numbers[i]);
+        for (String currentNumber:numbers) {
+            if (stringToInt(currentNumber) > 1000) {
+                continue;
+            }
+            sum += stringToInt(currentNumber);
         }
         return sum;
     }
     
-    private int getInt(String number) {
-        return Integer.parseInt(number);
+    private int stringToInt(String number) {
+        int num = Integer.parseInt(number);
+        if (num < 0) {
+            throw new IllegalArgumentException("Negative input!");
+        } 
+        else {
+            return num;
+        }
     }
 }
