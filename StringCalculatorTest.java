8ffca7c353f8369d3081709e208f82132237a149
divineo <59251464+divineo@users.noreply.github.com>
2020-06-01 14:04:32 +0530

    test for comma , and new line \n

--- StringCalculatorTest.java
+++ StringCalculatorTest.java
@@ -4,26 +4,32 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class StringCalculatorTest {
+    
+    private StringCalculator calculator;
+
+    @Before
+    public void initialize() {
+        calculator = new StringCalculator();
+    }
 
     @Test
     public void emptyStringShouldReturn0() {
-        StringCalculator calculator = new StringCalculator();
         assertEquals(calculator.add(""), 0);
     }
     
     @Test
     public void string1ShouldReturn1() {
-        StringCalculator calculator = new StringCalculator();
         assertEquals(calculator.add("1"), 1);
     }
     @Test
     public void string12ShouldReturn3() {
-        StringCalculator calculator = new StringCalculator();
         assertEquals(calculator.add("1 2"), 3);
     }
     @Test
     public void stringUnkownShouldReturnCorrect() {
-        StringCalculator calculator = new StringCalculator();
         assertEquals(calculator.add("1 2 3 4 5 6"), 21);
     }
+    @Test
+    public void stringNewLineCommaReturnCorrect() {
+        assertEquals(calculator.add("1\n2,3,4\n5,6"), 21);
 }
