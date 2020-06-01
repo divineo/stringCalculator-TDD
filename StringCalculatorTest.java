32e20219a3d9dc2777a0c1a4e1072619493871d9
divineo <59251464+divineo@users.noreply.github.com>
2020-06-01 15:05:12 +0530

    Adding test for -ve numbers and numbers > 1000

--- StringCalculatorTest.java
+++ StringCalculatorTest.java
@@ -2,10 +2,15 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 
 public class StringCalculatorTest {
     
     private StringCalculator calculator;
+    
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Before
     public void initialize() {
@@ -35,5 +40,19 @@ public class StringCalculatorTest {
     @Test
     public void stringNewLineCommaReturnCorrect() {
         assertEquals(calculator.add("1\n2,3,4\n5,6"), 21);
+    }
+    
+    @Test
+    public void negativeInputReturnsException() {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Negative input!");
+        calculator.add("-20");
+        calculator.add("-3,66\n-23");
+    }
+
+    @Test
+    public void numbersGreaterThan1000AreIgnored() {
+        assertEquals(calculator.add("4,2,1001"), 6);
+        assertEquals(calculator.add("26969,41\n39,8421"), 80);
     }
 }
