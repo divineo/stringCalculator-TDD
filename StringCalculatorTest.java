6770b7d6e92fb3924d6f41798b5613dd0b234caf
divineo <59251464+divineo@users.noreply.github.com>
2020-06-01 14:38:22 +0530

    Update StringCalculatorTest.java

--- StringCalculatorTest.java
+++ StringCalculatorTest.java
@@ -21,15 +21,19 @@ public class StringCalculatorTest {
     public void string1ShouldReturn1() {
         assertEquals(calculator.add("1"), 1);
     }
+    
     @Test
     public void string12ShouldReturn3() {
         assertEquals(calculator.add("1 2"), 3);
     }
+    
     @Test
     public void stringUnkownShouldReturnCorrect() {
         assertEquals(calculator.add("1 2 3 4 5 6"), 21);
     }
+    
     @Test
     public void stringNewLineCommaReturnCorrect() {
         assertEquals(calculator.add("1\n2,3,4\n5,6"), 21);
+    }
 }
